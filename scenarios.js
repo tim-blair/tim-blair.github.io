7c43c74daedd63e4a120d1662cdf77f44505cc94
Ruben <ruben@binsentry.com>
2020-04-11 09:28:26 -0400

    Move scenario configuration to javascript (add missing files)

---
+++ scenarios.js
@@ -0,0 +1,32 @@
+const scenarios = new Map();
+scenarios.set(1, {
+    alignment: 'horz',
+    map: {
+        G1b: {classes: ['revrotate'], style: {top: '144px', left: '400px'}},
+        I1b: {classes: ['rotate'], style: {top: '91px', left: '204px'}},
+        L1a: {classes: ['rotate'], style: {top: '423px', left: '487px'}},
+    },
+    start: [],
+    doors: []
+});
+scenarios.set(20, {
+    alignment: 'vert',
+    map: {
+        K1a: {style: {top: '304px', left: '289px'}},
+        J1a: {classes: ['rotate'], style: {top: '598px', left: '59px'}},
+        C1a: {style: {top: '658px', left: '554px', transform: 'scale(0.15)'}},
+        D1a: {style: {top: '0', left: '350px'}},
+    },
+    start: [
+        {top: '903px', left: '77px'},
+        {top: '955px', left: '49px'},
+        {top: '955px', left: '106px'},
+        {top: '955px', left: '165px'},
+        {top: '1006px', left: '136px'},
+    ],
+    doors: [
+        {top: '292px', left: '546px'},
+        {top: '647px', left: '400px'},
+        {top: '647px', left: '691px'},
+    ]
+});
